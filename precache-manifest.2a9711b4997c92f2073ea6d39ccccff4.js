c0f91829eba66de9a411df7b414f90c19b442f43
raibikys <70314658+raibikys@users.noreply.github.com>
2020-08-27 12:02:48 +0300

    Updates

---
+++ precache-manifest.2a9711b4997c92f2073ea6d39ccccff4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b81382483010cdf53fadc13523a8ece",
+    "url": "/react-youtube-app/index.html"
+  },
+  {
+    "revision": "1bbd50da3e959b815d07",
+    "url": "/react-youtube-app/static/css/main.823deb74.chunk.css"
+  },
+  {
+    "revision": "be7c4a04626882a5703b",
+    "url": "/react-youtube-app/static/js/2.c60faabe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-youtube-app/static/js/2.c60faabe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bbd50da3e959b815d07",
+    "url": "/react-youtube-app/static/js/main.2eaebad1.chunk.js"
+  },
+  {
+    "revision": "74aad2350f343b775670",
+    "url": "/react-youtube-app/static/js/runtime-main.4ecfd3fe.js"
+  },
+  {
+    "revision": "ef4f0661073d7e3b3c9b8186fb62e180",
+    "url": "/react-youtube-app/static/media/logo.ef4f0661.png"
+  }
+]);
